4b94b33ba6a78254c4a09be863abe67828fb3136
Gabriel Pinto <gl.pinto10@uniandes.edu.co>
2018-08-26 00:15:40 -0500

    Estructura básica. Incluye mi foto, nombre e iconos de contacto

---
+++ js/main.js
@@ -0,0 +1,3 @@
+const images = [
+  "https://by3301files.storage.live.com/y4pf8zv0F817dxw9RPt_L0zkeFefNkNXFoKaa8u3q7JHCQ4YY9DyDvtO4VEu9kQTURVF4PbKoD24sTUFE27jqLlZ9L25FHreCIDqMqiZTPcBIiI-f9S9-Wj9orDo1MxoOmIjovZ5NHSJuk1fkmeucVvu4jY9d2xktyDcUL4XqJjixCx_9PDH9mi1Vs2jc9MbD9P/GabrielPintoPineda.jpg?psid=1&width=960&height=960&cropMode=center"
+];
