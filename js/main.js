36938d7377ef5bc3d6c4ba1529f8870489cbf5f5
Gabriel Pinto <gl.pinto10@uniandes.edu.co>
2018-08-26 02:34:47 -0500

    Se agregan los links de contacto y frase personal

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
-const images = [
-  "https://by3301files.storage.live.com/y4pf8zv0F817dxw9RPt_L0zkeFefNkNXFoKaa8u3q7JHCQ4YY9DyDvtO4VEu9kQTURVF4PbKoD24sTUFE27jqLlZ9L25FHreCIDqMqiZTPcBIiI-f9S9-Wj9orDo1MxoOmIjovZ5NHSJuk1fkmeucVvu4jY9d2xktyDcUL4XqJjixCx_9PDH9mi1Vs2jc9MbD9P/GabrielPintoPineda.jpg?psid=1&width=960&height=960&cropMode=center"
-];
+// Amplia los iconos de redes al pasar sobre ellos
+$('.icono').hover(function () {
+  $(this).toggleClass('zoom-hover', 2000);
+});
